070ebfbd7f95ad5319b78ea6b64c54c8d315e36b
Jason Ma <jasonma062019@gmail.com>
2025-01-02 18:36:08 -0800

    Create notes button on sidebar

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -2,8 +2,27 @@ import React from "react";
 import Nav from "react-bootstrap/Nav";
 import icons from "../../assets/icons";
 import "./Sidebar.css";
+import axiosInstance from "../../utils/axiosInstance";
 
 const Sidebar = ({ userInfo, toggleSidebar }) => {
+  const [notes, setNotes] = React.useState(userInfo.notes || []);
+
+  const newPage = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axiosInstance.post("/api/notes/create");
+
+      if (response.data) {
+        console.log("New note created");
+        console.log(response.data);
+        console.log(notes);
+        setNotes((prevNotes) => [...prevNotes, response.data.note]); // Update notes state
+      }
+    } catch (error) {
+      console.error("Error creating new note:", error);
+    }
+  };
+
   return (
     <div className="sidebar-container">
       <div className="sidebar-header">
@@ -17,23 +36,24 @@ const Sidebar = ({ userInfo, toggleSidebar }) => {
       </div>
       <Nav className="sidebar-nav flex-column">
         <Nav.Link href="#home">Home</Nav.Link>
-        <button>test</button>
+        <button action="#" method="POST" onClick={newPage}>
+          test
+        </button>
         <Nav.Link href="#about">About</Nav.Link>
         <Nav.Link href="#services">Services</Nav.Link>
         <Nav.Link href="#contact">Contact</Nav.Link>
       </Nav>
       <div className="sidebar-notes">
         <h5>Your Notes</h5>
-         {userInfo.notes && userInfo.notes.length > 0 ? (
-          userInfo.notes.map((note, index) => (
+        {notes && notes.length > 0 ? (
+          notes.map((note, index) => (
             <li key={index} className="note-item">
-              {/* <Nav.Link href={`#note-${note._id}`}>{note.title}</Nav.Link> */}
-              <p>{ note }</p>
+              <p>{typeof note === "string" ? note : note._id}</p>
             </li>
           ))
-          ) : (
-            <p>No notes found</p>
-          )}
+        ) : (
+          <p>No notes found</p>
+        )}
       </div>
     </div>
   );
